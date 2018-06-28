5d7e271f185a744528411e5be7c68cade2ac5a13
Dang Thanh Hao <aszqsc@gmail.com>
2018-06-29 02:17:23 +0700

    a

---
+++ SalesFood/SalesFood/Report.aspx.cs
@@ -0,0 +1,30 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SalesFood
+{
+    public partial class WebForm10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*ReportDocument reportDocument = new ReportDocument();
+
+            reportDocument.Load(Server.MapPath("CrystalReport1.rpt"));
+            
+            reportDocument.Database.Tables[0].SetDataSource(new DataSet1());
+            
+            //reportDocument.SetDatabaseLogon("sa", "suhao123", ".", "SalesFood");
+
+            CrystalReportViewer1.ReportSource = reportDocument;
+            */
+            CrystalReportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ SalesFood/SalesFood/ReportQuantity.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ProductManager.master" AutoEventWireup="true" CodeBehind="WebForm10.aspx.cs" Inherits="SalesFood.WebForm10" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeaderProductManager" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="FooterProductManager" runat="server">
+</asp:Content>
